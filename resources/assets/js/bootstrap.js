b6fdeafbf4eadcd16d5c2450a39167d0e4b28e4e
LittleElijah <cuiyeqing960904@gmail.com>
2018-10-14 00:15:39 +0300

    packages installed

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -10,6 +10,9 @@ window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
 
+import Vuex from 'vuex';
+Vuex.use(Vuex);
+
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
  * using reactive data binding and reusable components. Vue's API is clean
